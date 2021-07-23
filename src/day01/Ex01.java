d0588178615ff600e59f56d909806c26d79b0119
sungtaek <ohbass95@gmail.com>
2021-07-23 11:19:09 +0900

    주석방법

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -2,8 +2,16 @@ package day01;
 
 public class Ex01 {
 	public static void main(String[] args) {
+		// sysout 는 괄호안의 내용을 출력하는 기능
+		//;(쎄미콜론)는 문장의 끝을 알리는 용도
+//	팀플을 할때 부가적인 설명이 필요할때에도 자주 사용한다
+		/*문단주석입니다*/
+		/*
 		System.out.println("안녕하세요");
 		System.out.println("자바시간입니다");
+		*/
 		
+		System.out.print("안녕하세요");
+		System.out.print("자바 시간입니다");
 	}
 }
