33ab7ad3a89da4a028bcb4784b0eeb6877ec7bc1
sungtaek <ohbass95@gmail.com>
2021-07-23 12:49:13 +0900

    문자열 기능

--- src/day01/Ex02.java
+++ src/day01/Ex02.java
@@ -4,11 +4,14 @@ public class Ex02 {
 	public static void main(String[] args) {
 		/*
 		 *\n : 엔터와 비슷하다
-		 *\t :탭크기만큼 오른쪽으로 이동(윈도우에서는 대략 8칸 정도)
+		 *\t :탭크기만큼 오른쪽으로 이동(윈도우에서는 대략 스페이스바 8칸 정도)
 		 *\" : "(큰따옴표)표시 (일반 문자로 사용하고자 할때)
 		 *\\ : \(역슬래쉬) 표현할때 사용
-		 
+		 // "" :쌍따옴표로 묶인 값은 문자열로 인식한다
 		 * */
+	System.out.println("H\ne\nl\nlo \njava");
+	System.out.println("1\t1234567\t12345678\t12");
+	System.out.println("test \"123"\ \\456");
 	}
 
 }
